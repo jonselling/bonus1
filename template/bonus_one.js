e96ba0a19aed1ecf3edf6ff4c3932626064e4d63
Jon Selling <jonselling2317@aol.com>
2016-03-28 15:37:35 -0400

    work on the project, submission is ready

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -11,7 +11,49 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
+
 	// Magic!
-	console.log('Keepin\'n it clean with an external script!');
-})();
+	
+var InterestArray = new Array();
+var ProgArray = new Array();
+var FullArray = new Array();
+var MyArray = new Array();
+$.getJSON("http://www.mattbowytz.com/simple_api.json?data=all", function(my_data){
+	InterestArray = my_data.data.interests;
+	ProgArray = my_data.data.programming;
+	for(var i = 0; i < InterestArray.length; i++)
+	{
+		FullArray.push(InterestArray[i]);
+	}
+	for(var i = 0; i < ProgArray.length; i++)
+	{
+		FullArray.push(ProgArray[i]);
+	}
+	document.addEventListener("keyup", myfunction);
+	MyArray = FullArray;
+});
+
+console.log('Keepin\'n it clean with an external script!');
+
+function myfunction(){
+	var input;
+	var found;
+	input = document.getElementById("searchText").value;
+	document.getElementById("searchList").innerHTML = "";
+	
+	var len = input.length;
+	for(var i = 0; i < MyArray.length; i++)
+	{
+		found = false;
+		if(MyArray[i].substring(0,len).toLowerCase() == input.toLowerCase() && len != 0)
+			found = true;
+		else
+			found = false;
+		
+		if(found == true)
+		{
+			var add = MyArray[i];
+			$("#searchList").append('<li><a href="https://www.google.com/#q='+add+'">'+ add + '</a></li>');
+		}
+	}
+}
